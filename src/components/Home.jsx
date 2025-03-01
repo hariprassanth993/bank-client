4c1a7cc3fc042cab3122acbd5b60eb91746573fc
hariprassanth993 <hariprassanth773@gmail.com>
2025-03-01 19:01:01 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+
+import { useState, useContext, useEffect } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+// import Register from './register';
+// import Deposit from './deposit';
+// import Cashback from './cashback';
+// import Alldata from './alldata';
+// import Loading from './loding';
+// import Login from './login';
+import Navbar from './navbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/nav.css';
+
+import { navdataContext } from './login';
+
+function Home() {
+
+  return (
+    <>
+    <h1>Baskaran Bank</h1>
+    </>
+  );
+}
+
+export default Home;
